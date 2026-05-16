e5207b4ab8057840cec0583ff98083fd1151eb33
Piyush Singhal <56195693+piyush0899@users.noreply.github.com>
2026-05-17 02:41:35 +0530

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+   public int reverse(int x) {
+
+        int rev = 0;
+
+        while (x != 0) {
+
+            int digit = x % 10;
+
+            // overflow check
+            if (rev > Integer.MAX_VALUE / 10 ||
+               (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+
+            // underflow check
+            if (rev < Integer.MIN_VALUE / 10 ||
+               (rev == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+
+            x = x / 10;
+        }
+
+        return rev;
+    }
+}
