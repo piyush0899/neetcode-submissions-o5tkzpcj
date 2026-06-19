44ab6b54ef2aabcc4e556b2dc01a8547a390f68e
Piyush Singhal <56195693+piyush0899@users.noreply.github.com>
2026-06-19 17:39:16 +0530

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.java
@@ -0,0 +1,53 @@
+class Solution {
+   public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        Set<String> dict = new HashSet<>(wordList);
+
+        if (!dict.contains(endWord))
+            return 0;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+
+        int level = 1;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+
+                String word = queue.poll();
+
+                if (word.equals(endWord))
+                    return level;
+
+                char[] chars = word.toCharArray();
+
+                for (int j = 0; j < chars.length; j++) {
+
+                    char original = chars[j];
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+
+                        chars[j] = c;
+
+                        String next = new String(chars);
+
+                        if (dict.contains(next)) {
+
+                            queue.offer(next);
+                            dict.remove(next);
+                        }
+                    }
+
+                    chars[j] = original;
+                }
+            }
+
+            level++;
+        }
+
+        return 0;
+    }
+}
