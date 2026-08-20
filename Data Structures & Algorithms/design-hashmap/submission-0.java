920c934d45dc7ee149459e266908823799643b63
Piyush Singhal <56195693+piyush0899@users.noreply.github.com>
2026-08-21 00:58:33 +0530

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.java
@@ -0,0 +1,24 @@
+class MyHashMap {
+
+    private int[] map;
+
+    public MyHashMap() {
+        map = new int[1_000_001];
+
+        for (int i = 0; i < map.length; i++) {
+            map[i] = -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+
+    public int get(int key) {
+        return map[key];
+    }
+
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
