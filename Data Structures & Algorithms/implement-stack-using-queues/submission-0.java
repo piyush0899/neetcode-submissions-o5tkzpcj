c59515ba4c20e77b1dde923ff76ae1ea487ad32f
Piyush Singhal <56195693+piyush0899@users.noreply.github.com>
2026-09-14 20:09:02 +0530

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.java
@@ -0,0 +1,43 @@
+
+
+class MyStack {
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+
+        // New element q2 mein daalo
+        q2.offer(x);
+
+        // q1 ke saare elements q2 mein transfer karo
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
+        }
+
+        // q1 aur q2 swap karo
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    public int pop() {
+
+        return q1.poll();
+    }
+
+    public int top() {
+
+        return q1.peek();
+    }
+
+    public boolean empty() {
+
+        return q1.isEmpty();
+    }
+}
